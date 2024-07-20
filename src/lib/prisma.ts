ed8a763d4508bcae0af3ed18163189fad5f812c8
BoschiAlbano <iprogramacion17@gmail.com>
2024-07-20 19:12:07 -0300

    PrismaInstanciaError

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,37 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//     return new PrismaClient();
+// };
+
+// declare const globalThis: {
+//     prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-    return new PrismaClient();
-};
+// Extiende la interfaz global para incluir la propiedad `prisma`
+declare global {
+    // Para evitar errores de "no-var"
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient | undefined;
+}
 
-declare const globalThis: {
-    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+let prisma: PrismaClient;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
